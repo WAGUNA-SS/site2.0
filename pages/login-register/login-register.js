cdcc99a2c4eefc888575b4b3ba8a6209b414cedf
WAGUNA <111994349+WAGUNA-SS@users.noreply.github.com>
2024-01-16 08:25:21 -0300

    Update login-register.js

--- pages/login-register/login-register.js
+++ pages/login-register/login-register.js
@@ -2,7 +2,7 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 
-registerLink.addEventListener( 'click',()=> {
+registerLink.addEventListener( 'click', ()=> {
     wrapper.classList.add('active');
 });
 
